de4fca5bee1717bf757d28cd0f78c5c65773e394
Nikkirps7 <nikkirps7@gmail.com>
2018-04-11 23:43:29 +0530

    commit

---
+++ data_structures.R
@@ -0,0 +1,24 @@
+#data structures in R
+
+#Vectors----
+v1 = c('A','B')
+v1
+v2 = c(1,2,3)
+v2
+class(v2)
+v3 = 1:10
+
+v3[v3 %in% v2]
+
+
+#Matrix----
+
+
+
+#Arrays----
+
+#List----
+
+#Factors----
+
+#DataFrame----
